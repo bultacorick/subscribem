24808e4dc6553badd83c5479bc4ea9154128e1a8
Rick Lloyd <richard.t.lloyd@gmail.com>
2013-10-28 15:09:21 -0400

    Implemented code from Section 2.5

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -1,6 +1,20 @@
 module Subscribem
   class Account < ActiveRecord::Base
+    EXCLUDED_SUBDOMAINS = %w(admin)
+    validates :subdomain, exclusion: {
+      :in => EXCLUDED_SUBDOMAINS,
+      :message => 'is not allowed. Please choose another subdomain.'
+    }
+    validates :subdomain, format: {
+      :with => /\A[\w\-]+\z/i,
+      :message => 'is not allowed. Please choose another subdomain.'
+    }
+    validates :subdomain, :presence => true, :uniqueness => true
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
+
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
   end
 end
