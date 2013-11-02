5ed4009a69c979408ea50e90b108cab0a5154956
Rick Lloyd <richard.t.lloyd@gmail.com>
2013-11-01 20:55:43 -0400

    Implementing code in Section 3.1

--- lib/subscribem/engine.rb
+++ lib/subscribem/engine.rb
@@ -7,6 +7,13 @@ module Subscribem
     config.generators do |g|
       g.test_framework :rspec, :view_specs => false
     end
+    config.to_prepare do
+      root = Subscribem::Engine.root
+      extenders_path = root + "app/extenders/**/*.rb"
+      Dir.glob(extenders_path) do |file|
+        Rails.configuration.cache_classes ? require(file) : load(file)
+      end
+    end
     initializer 'subscribem middleware warden' do
       Rails.application.config.middleware.use Warden::Manager do |manager|
         manager.default_strategies :password
