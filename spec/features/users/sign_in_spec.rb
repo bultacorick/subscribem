47bc65412813552a71a34541d600077696a35011
Rick Lloyd <richard.t.lloyd@gmail.com>
2013-11-05 22:00:56 -0500

    Implementing code from Section 4.2

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
+require 'subscribem/testing_support/subdomain_helpers'
+require 'subscribem/testing_support/factories/account_factory'
+require 'subscribem/testing_support/factories/user_factory'
 feature 'User sign in' do
-  extend SubdomainHelpers
+  extend Subscribem::TestingSupport::SubdomainHelpers
   let!(:account) { FactoryGirl.create(:account_with_schema) }
   let(:sign_in_url) { "http://#{account.subdomain}.example.com/sign_in" }
   let(:root_url) { "http://#{account.subdomain}.example.com/" }
